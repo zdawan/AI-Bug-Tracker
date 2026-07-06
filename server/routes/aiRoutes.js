2a8133f445f5fd0e1e0c3f24fe880057c7588b1b
zdawan <kumarjothi704@gmail.com>
2026-07-06 21:29:03 +0530

    Fix Puppeteer executable path

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -27,13 +27,14 @@ router.post("/analyze", async (req, res) => {
 
   try {
     browser = await puppeteer.launch({
-  headless: true,
-  args: [
-    "--no-sandbox",
-    "--disable-setuid-sandbox",
-    "--disable-dev-shm-usage",
-  ],
-});
+      executablePath: puppeteer.executablePath(),
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+      ],
+    });
 
     const page = await browser.newPage();
     await page.goto(websiteUrl, { waitUntil: "networkidle2", timeout: 30000 });
@@ -148,7 +149,7 @@ ${pageText}
     if (browser) {
       try {
         await browser.close();
-      } catch (_) {}
+      } catch (_) { }
     }
 
     return res.status(500).json({
